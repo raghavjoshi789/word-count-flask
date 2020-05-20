00f577d4740aeee5ecf91f6cbd83c985a152faa7
Raghav Joshi <raghavjoshi789@gmail.com>
2020-05-20 18:17:24 +0530

    testing config variables

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+import os
 from flask import Flask
 
+
 app = Flask(__name__)
+
+
+print(app.config['DEBUG'])
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
 
 @app.route('/<name>')
 def hello(name):
