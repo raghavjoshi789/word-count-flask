dc6be921bb79b2519568148c773e9d9641c198b8
Raghav Joshi <raghavjoshi789@gmail.com>
2020-05-20 17:10:00 +0530

    playing with it

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+@app.route('/<name>')
+def hello(name):
+    return f"Hello, {name}!"
 
 if __name__ == "__main__":
     app.run()
